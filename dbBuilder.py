e97b2595302d51f7b9afb935526cf091f1a5f2ea
craig131 <craig.nagy131@gmail.com>
2015-09-27 16:45:44 -0600

    DB generation thrown into hyperdrive with sqlite3 implementation instead of dataset

--- dbBuilder.py
+++ dbBuilder.py
@@ -1,26 +1,24 @@
-import time
-import dataset
+import sqlite3
 
-db = dataset.connect('sqlite:///adobepws.db')
-table = db['adobepws']
+conn = sqlite3.connect('adb.db')
+c = conn.cursor()
+
+c.execute('''CREATE TABLE adb
+		(passwd TEXT, hint TEXT, email TEXT)''')
 
 #Stage 1: Creating and pickling [password, hint] reference list:
 print " [+] Reading list of encrypted passwords file..."
-stage1start = time.time()
-with open('cred') as openfileobject:
-	pass_and_hint = []
-	print " [+] Building database. Go take a walk, this will take a long time."
-	for line in openfileobject:
-       		pipe_removal = line.split('|-')
-		if len(pipe_removal) == 6:
-			email = pipe_removal[2][:-1] #[:-1] removes last '-' char from email str
-			passwd = pipe_removal[3][:-3] #[:-3] removes last three "==-" chars from passwd str
-			hint = pipe_removal[4]
-			if len(hint) > 0: #If hint is empty, ignore line
-				#pass_and_hint.append([passwd, hint, email])
-				table.insert(dict(passwd=passwd, hint=hint, email=email))
-stage1end = time.time()
-print "~" + str(int((stage1end - stage1start)/60)) + " minutes"
+cred = open('cred')
+print " [+] Building database. Go take a walk, this will take a long time."
+for line in cred:
+	pipe_removal = line.split('|-')
+	if len(pipe_removal) == 6: 
+		passwd = pipe_removal[3][:-3]
+		hint = pipe_removal[4]
+		email = pipe_removal[2][:-1]
+		if len(hint) > 0:
+			c.execute("INSERT INTO adb (passwd, hint, email) VALUES (?, ?, ?)",
+				(passwd, hint, email))
 
-
-
+conn.commit()
+conn.close()
